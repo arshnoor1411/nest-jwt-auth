df78ba31a19d9888283d23ab73ac428b2e8bd63a
arshnoor1411 <arshnoor@infocusp.com>
2025-04-07 21:56:20 +0530

    enhance: user migration added

---
+++ migrations/1744029440670-migration-name.ts
@@ -0,0 +1,75 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class MigrationName1744029440670 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'first_name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'last_name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'is_email_verified',
+            type: 'boolean',
+            isNullable: false,
+          },
+          {
+            name: 'otp',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'otp_sent_at',
+            type: 'timestamp',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "users"`);
+  }
+}
